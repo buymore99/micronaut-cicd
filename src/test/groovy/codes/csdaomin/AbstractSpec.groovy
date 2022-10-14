7f0857a491ea24a386c82595087d8aa7c8cb38fb
Matthew Huang Meirong <matthew.huangmr@shopee.com>
2022-10-14 23:17:47 +0800

    add an action to run tests

---
+++ src/test/groovy/codes/csdaomin/AbstractSpec.groovy
@@ -0,0 +1,10 @@
+package codes.csdaomin
+
+import io.micronaut.context.ApplicationContext
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+public class AbstractSpec extends Specification {
+    
+}
